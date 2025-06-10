59143e0b6da7c855898f2c95b6e1c775301ea5a7
BLACK1895 <miguel18.jmr13@gmail.com>
2025-06-10 03:56:13 -0300

    se hizo index.pug, style pug, app.js y se logro conexion con el host

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Bienvenido al HIS' });
+});
+app.use((req, res, next) => {
+    res.status(404).send('Página no encontrada');
+});
+app.use((err, req, res, next) => {
+    res.status(404).send('Página no encontrada');
+    console.error(err.stack);
+    res.status(500).send('Algo salió mal!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
