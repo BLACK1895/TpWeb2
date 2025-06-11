af81507bc4bd07e855254c5882477a3bea891483
BLACK1895 <miguel18.jmr13@gmail.com>
2025-06-10 23:37:19 -0300

    Update db.js

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2/promise');
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root', 
+    password: 'root',
+    database: 'hospital_db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+module.exports = pool;
