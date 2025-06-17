c49792f3fdf309dbf252dd806232ceeccf11552c
BLACK1895 <miguel18.jmr13@gmail.com>
2025-06-17 20:56:36 -0300

    s

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,3 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Pacientes', [
@@ -27,6 +25,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Pacientes', null, {}); // Borra todos los pacientes
+    await queryInterface.bulkDelete('Pacientes', null, {}); 
   }
 };
