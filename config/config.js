e61a08539d51ded40b7381327acae5b9548ec5d9
BLACK1895 <miguel18.jmr13@gmail.com>
2025-06-18 00:40:35 -0300

    s

--- config/config.js
+++ config/config.js
@@ -13,14 +13,13 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "production": {
+    "production": {
     "username": process.env.DB_USER,      
     "password": process.env.DB_PASSWORD,   
-    "database": process.env.DB_NAME,      
+    "database": process.env.DB_NAME,       
     "host": process.env.DB_HOST,         
-    "port": process.env.DB_PORT,    
+    "port": process.env.DB_PORT,         
     "dialect": "mysql",
     "logging": false
-  
   }
 };
--- models/paciente.js
+++ models/paciente.js
@@ -4,23 +4,11 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Paciente extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
   }
   Paciente.init({
-    id_paciente: { // <-- Asegúrate de que tu clave primaria se llame así
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false
-    },
-    // ... el resto de tus atributos
     nombre: DataTypes.STRING,
     apellido: DataTypes.STRING,
     fecha_nacimiento: DataTypes.DATE,
@@ -33,8 +21,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Paciente',
-    tableName: 'Pacientes', // Asegúrate de que el nombre de la tabla sea correcto
-    underscored: true // Si usas snake_case en tu DB como id_paciente, esto es útil
+    tableName: 'Pacientes',
+    underscored: true
   });
   return Paciente;
 };
